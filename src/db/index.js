86c9c1ce3b7643c06eace67e4dfd7a00015d6b4f
Kishore <167499996+ltkcl@users.noreply.github.com>
2025-10-25 10:22:44 +0530

    util was added

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant";
+ 
+const connectDB = async ()=>{
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    }catch(err){
+        console.error("MongoDB connection Error : ",err);
+        process.exit(1);
+      
+    }
+}
+export default connectDB
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+const asyncHandler= (requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>{
+            next(err);
+        })
+    }
+}
+
+export { asyncHandler}
+// const asyncHandler = (fn)=> async (req,res,next)=>{
+//     try{
+//         await fn(req,res,next)
+//     }catch(err){
+//         res.status(err.status|| 5000).json({      
+//             success: false,
+//             message: err.message      
+//         })
+//     }
+// } 
